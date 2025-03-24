bf808d9e32841077a81719e8b685e9a864e0f246
Fatoki Lawrence <93355867+jydestudio@users.noreply.github.com>
2025-03-24 15:20:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const cors = require("cors");
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
